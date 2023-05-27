36497136cfbe66a59b5c7cca42535ab129b94446
Kirill Nesmeyanov <nesk@xakep.ru>
2023-05-28 00:29:26 +0300

    Fix entrypoint php file name

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     PHP_EMBED_START_BLOCK(argc, argv)
 
     zend_file_handle file_handle;
-    zend_stream_init_filename(&file_handle, "entrypoint.vendor");
+    zend_stream_init_filename(&file_handle, "entrypoint.php");
     php_execute_script(&file_handle);
 
     PHP_EMBED_END_BLOCK()
